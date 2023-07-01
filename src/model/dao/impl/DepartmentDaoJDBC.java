18b44579acdf9e489dd0b57f54d69f3a50900453
Gabriel Resende <gabriellrr2005@gmail.com>
2023-06-30 21:32:41 -0300

    DepartmentForm (dialog) design

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -27,7 +27,7 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement("SELECT * FROM department WHERE Id = ?");
+			st = conn.prepareStatement("SELECT * FROM de partment WHERE Id = ?");
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if (rs.next()) {
